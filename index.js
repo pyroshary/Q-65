13a3163d86ebc286b8c69d54ce406c72909b4d22
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-03 00:53:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function Remainder(num1, num2) {
+    return num1 % num2;
+}
+console.log(Remainder(9, 2));
---
+++ index.ts
@@ -0,0 +1,7 @@
+//making a function of 2 number dividing which each other
+function Remainder(num1:number, num2: number) :  number {
+    return num1 % num2
+}
+
+
+console.log(Remainder(9,2));
